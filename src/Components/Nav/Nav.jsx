71202135434cf4366d770b2d55193ceefc232f08
Ezequiel Migueles <ezequielmigueles@icloud.com>
2021-10-07 01:00:09 +0200

    nav styled

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -3,19 +3,12 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav className="nav">
-      <ul>
-        <li className="nav-element">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="nav-element">
-          <Link to="/new">Add Pokemon</Link>
-        </li>
-        <li className="nav-element">
-          <Link to="/search">Finder</Link>
-        </li>
-      </ul>
-    </nav>
+    <div id="links">
+      <Link to="/">Home</Link>
+      <Link to="/new">Add Pokemon</Link>
+      <Link to="/search">Finder</Link>
+      <Link to="mailto:info@ichicodes.com">Contact</Link>
+    </div>
   );
 };
 
