2e1a14e3b26461f29e75e9b0a3081d1de00a5d4c
Ezequiel Migueles <ezequielmigueles@icloud.com>
2021-10-07 00:59:40 +0200

    header created styles

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,33 @@
-import React, { Component } from "react";
+import React from "react";
 import Nav from "../Nav";
-class Header extends Component {
-  render() {
-    return <div><Nav /></div>;
-  }
-}
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div id="navbar">
+          <div id="logo" className="reverse">
+            <div
+              className="mobile-btn"
+              onclick="openNav()"
+            >
+              &#9776;
+            </div>
+            <div className="logo">
+              Gr<span>id</span> CSS
+            </div>
+          </div>
+          <Nav />
+        </div>
+      </nav>
+      <div id="mySidenav" className="sidenav">
+        <button className="closebtn" onclick="closeNav()">
+          &times;
+        </button>
+        <Nav />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
