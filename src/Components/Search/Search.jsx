997e1356680218d032ef7642bc10fc3bf81b8809
Ezequiel Migueles <ezequielmigueles@icloud.com>
2021-10-06 16:37:16 +0200

    search fixed

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -3,37 +3,18 @@ import Form from "../Form";
 import Card from "../Card";
 
 const Search = () => {
-  const [data, setData] = useState("");
   const [pokemon, setPokemon] = useState([]);
-  const [filteredPokemon, setFilteredPokemon] = useState([]);
 
   const childToParent = async (childdata) => {
-    await setData(childdata);
+    await setPokemon(childdata);
   };
-
-  useEffect(() => {
-    if (data) {
-      setPokemon([...pokemon, data]);
-    }
-  }, [data]);
-
-  useEffect(() => {
-    if (pokemon) {
-      const uniqueArray = Array.from(new Set(pokemon.map((a) => a.id))).map(
-        (id) => {
-          return pokemon.find((a) => a.id === id);
-        }
-      );
-      setFilteredPokemon(uniqueArray);
-    }
-  }, [pokemon]);
 
   return (
     <div className="home">
       <Form childToParent={childToParent} />
       <div className="home__cards">
-        {filteredPokemon.map((filteredPokemon) => (
-          <Card key={filteredPokemon.name} pokemon={filteredPokemon} />
+        {pokemon.map((pokemon) => (
+          <Card key={pokemon.name} pokemon={pokemon} />
         ))}
       </div>
     </div>
