9a1004b306c02876ffe3de41afe253e6a938a556
Ezequiel Migueles <ezequielmigueles@icloud.com>
2021-10-07 03:45:00 +0200

    pokeball shining and menu fixed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      Copyright &copy; {new Date().getFullYear()} Ezequiel Migueles Abraira
+      MIT &copy; {new Date().getFullYear()} Ezequiel Migueles Abraira
     </footer>
   );
 };
